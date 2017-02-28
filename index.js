bcf8ce1c3718562fd6999037009dd6758aaa329c
Evan Moncuso <evan.moncuso@gmail.com>
2017-02-27 21:31:13 -0500

    [Build]: add the command line actions to make it do something

---
+++ index.js
@@ -0,0 +1,96 @@
+#!/usr/bin/env node
+
+const program = require('commander');
+const shell = require('shelljs');
+
+const parts = require('./template-files/parts');
+
+const _mkPath = (path) => {
+  pathArr = path.split('/')
+
+  pathArr.forEach((i) => {
+    if(shell.ls().indexOf(i) === -1) {
+      // mkdir
+      shell.mkdir(i);
+    }
+    // cd into dir
+    shell.cd(i);
+  })
+}
+
+
+const _buildPod = (podname, program) => {
+  let pod = {
+    podName: podname,
+    files: ['index.js', 'styles.js'],
+    type: 'smart',
+    redux: false,
+    indexDetails: {
+      hasMSTP: false,
+      hasMDTP: false,
+    }
+  }
+
+  // should the pods be a dumb component?
+  if(program.dumbComponent) {
+    pod.type = 'dumb';
+  }
+  // should the pod connect to redux store?
+  if(program.redux) {
+    pod.redux = true;
+    // mapStateToProps?
+    if(program.mapStateToProps) {
+      pod.indexDetails.hasMSTP = true;
+    }
+    // mapDispatchToProps?
+    if(program.mapDispatchToProps) {
+      pod.indexDetails.hasMDTP = true;
+    }
+    // actions?
+  }
+
+  if(program.addActions) {
+    pod.files.push('actions.js');
+  }
+
+  return pod;
+}
+
+program
+  .arguments('<podname>')
+  .option('-r, --redux', 'has access to the redux store')
+  .option('-a, --add-actions', 'include an actions file')
+  .option('-s, --mapStateToProps', 'include a mapStateToProps function and connect the index.js file to the redux store')
+  .option('-d, --mapDispatchToProps', 'include a mapDispatchToProps function and connect the index.js file to the redux store')
+  .option('-u, --dumb-component', 'make the component a \'dumb\' or presentational component with no independent state')
+  .parse(process.argv);
+
+const newIndex = parts.b(_buildPod(program.args, program));
+
+let pod = program.args[0];
+if(pod.slice(-1) === '/') {
+  pod = pod.slice(0, -1);
+}
+if(pod.slice(0, 1) === '/') {
+  pod = pod.slice(1);
+}
+
+let podname = pod.split('/').slice(-1)[0];
+let podpath = pod.split('/').slice(0, -1).join('/');
+
+console.log(pod, podpath, podname);
+
+// make a directory
+_mkPath(pod);
+
+// create the files
+shell.touch('index.js');
+shell.ShellString(newIndex).to('index.js');
+
+shell.touch('styles.js');
+shell.ShellString(parts.s).to('styles.js');
+
+if(program.addActions) {
+  shell.touch('actions.js');
+  shell.ShellString(parts.a).to('actions.js');
+}
---
+++ template-files/parts.js
@@ -0,0 +1,151 @@
+const stylePage = `import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+
+})
+`;
+
+const actionsPage =`/**
+  A nice example action obj
+
+  const dispatchXYZ = (data) => {
+    return {
+      type: 'AN_ACTION',
+      data: data,
+    }
+  }
+*/
+
+export const actions = {
+
+}
+`
+
+const imports = `import React, { Component } from 'react';
+import {
+  Text,
+  StyleSheet,
+  View,
+} from 'react-native';
+
+`
+
+const importStyles = `import { styles } from './styles';
+
+`;
+
+const importActions = `import { actions } from './actions';
+`;
+
+const connect = `import { connect } from 'react-redux';
+
+`;
+
+const _dumbInit = (podName) => (
+`const ${podName} = () => {
+  return (
+
+  );
+}
+
+`);
+
+const _smartInit = (podName) => (
+  `class ${podName} extends Component {
+  constructor(props, context) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+
+    )
+  }
+}
+
+`)
+
+const mstp = `const mapStateToProps = (state) => (
+  {
+
+  }
+);
+
+`
+
+const mdtp = `const mapDispatchToProps = (dispatch) => (
+  {
+
+  }
+);
+
+`
+
+const _connection = (mstp, mdtp, podName) => {
+  mstp = mstp ? 'mapStateToProps' : null;
+  mdtp = mdtp ? 'mapDispatchToProps' : null;
+  return `
+export default connect(${mstp}, ${mdtp})(${podName});
+  `
+}
+
+const _exportWithoutConnection = (podName) => (`export default ${podName};`);
+
+const buildPages = (buildObj) => {
+  let index = ``;
+  let styles = stylePage;
+  let { podName, type, redux, indexDetails, files } = buildObj;
+  let { hasMSTP, hasMDTP } = indexDetails;
+  let actions = files.length === 3;
+
+  let filename = podName[0].split('/');
+  if(filename[filename.length - 1] === '') {
+    filename = filename[filename.length - 2];
+  } else {
+    filename = filename[filename.length - 1];
+  }
+
+  // build the index page;
+
+  // start with imports;
+  index += imports;
+  // see if we need redux
+  if(redux) {
+    index += connect;
+  }
+  // add actions if needed
+  if(actions) {
+    index += importActions;
+  }
+  // add styles if needed
+  index += importStyles;
+
+  // check for component type
+  if(type === 'smart') {
+    index += _smartInit(filename);
+  } else {
+    index += _dumbInit(filename);
+  }
+
+  if(redux) {
+    hasMSTP = true;
+    index += mstp;
+
+    if(hasMDTP) {
+      index += mdtp;
+    }
+  }
+
+  if(redux) {
+    index += _connection(hasMSTP, hasMDTP, filename)
+  } else {
+    index += _exportWithoutConnection(filename);
+  }
+
+  return index;
+}
+
+module.exports = { b: buildPages, s: stylePage, a: actionsPage };
